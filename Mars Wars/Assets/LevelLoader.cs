3714a1637ff25ea1443d982c790a46442853adf5
Javiigh <javiigh2001@gmail.com>
2021-10-19 20:34:22 +0100

    T CA 19/10
    -Mejora en animación de texto
    -Creación de las transiciones (Incluye):
     +Elementos animados de transición.
     +Script para la transición.
    -Mejoras varias.

---
+++ Mars Wars/Assets/LevelLoader.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    public Animator transition;
+    public float Timer = 0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            LoadNextLevel();
+        }
+
+        Timer = Timer + Time.deltaTime;
+        Debug.Log(Timer);
+
+        if(Timer >= 90f)
+        {
+            LoadNextLevel();
+        }
+
+    }
+
+    public void LoadNextLevel()
+    {
+        StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex + 1));
+    }
+
+    IEnumerator LoadLevel(int LevelIndex)
+    {
+        //Play animation
+        transition.SetTrigger("Start");
+
+        //Wait
+        yield return new WaitForSeconds(1);
+
+        //Load scene
+        SceneManager.LoadScene(LevelIndex);
+    }
+}
