055f389343bb3f261c3781572935ba9dd88b5889
Javiigh <javiigh2001@gmail.com>
2021-11-12 13:23:44 +0000

    T CA 12/11
    +Avance en el proyecto Mars Wars:
     -Creación de los botones de guardar y cancelar
     -Creación de las distintas traducciones.
    -Etc.

---
+++ Mars Wars/Assets/Scripts/AudioController.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AudioController : MonoBehaviour
+{
+    public Slider SliderSound;
+    public AudioSource musicSource;
+    public AudioSource efectsSource;
+    public Slider SliderEffects;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        musicSource.volume = SliderSound.value;
+        //O SliderSoundMofied()
+        musicSource.pitch = SliderEffects.value;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("Valor modificado" + SliderSound.value);
+        Debug.Log("Valor modificado" + SliderEffects.value);
+    }
+
+    public void SliderSoundMofied()
+    {
+        Debug.Log("Valor modificado" + SliderSound.value);
+
+        //El volumen = slider sound value
+        musicSource.volume = SliderSound.value;
+    }
+
+    //Crear una función que al modificar el valor del slider de brillo/pitch cambia el pitch en el audio source
+
+    public void SliderEffectsMofied()
+    {
+        Debug.Log("Valor modificado" + SliderEffects.value);
+
+        //El pitch = slider sound value
+        musicSource.pitch = SliderEffects.value;
+    }
+}
