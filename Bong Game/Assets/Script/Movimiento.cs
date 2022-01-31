8870b96d41e71a5eb818238effc45b155920812e
Javiigh <javiigh2001@gmail.com>
2022-01-31 12:03:13 +0000

    T CL 31/01
    -Trabajo en Bong

--- Bong Game/Assets/Script/Movimiento.cs
+++ Bong Game/Assets/Script/Movimiento.cs
@@ -11,9 +11,9 @@ public class Movimiento : MonoBehaviour
     public GameObject Cubo;
     public GameObject ParedIz;
     public GameObject ParedDer;
-    public float movSpeed = 2f;
-    public float xrange = 10;
-    public float yrange = 10;
+    public float movSpeed = 1f;
+    public float xrange = 1;
+    public float yrange = 1;
     public float zrange = 0;
 
 
@@ -31,15 +31,15 @@ public class Movimiento : MonoBehaviour
     void Update()
     {
         //transform.Translate(Vector3.right * Time.deltaTime * movSpeed);
-        //transform.Translate(xrange * Time.deltaTime * movSpeed, yrange * Time.deltaTime * movSpeed, 0) ;
+        transform.Translate((xrange * Time.deltaTime * movSpeed)/40, (yrange * Time.deltaTime * movSpeed)/40, 0) ;
 
-        transform.Translate(1 * Time.deltaTime * movSpeed, 0, zrange*0);
+        //transform.Translate(1 * Time.deltaTime * movSpeed, 0, zrange*0);
 
         Debug.Log(xrange);
         Debug.Log(yrange);
         Debug.Log(zrange);
+        Debug.Log("mov " + movSpeed);
 
-        
     }
 
     void RamdonPosition()
@@ -49,7 +49,7 @@ public class Movimiento : MonoBehaviour
        
     }
 
-    public void OnTriggerEnter(Collider other)
+    private void OnTriggerEnter(Collider other)
     {
         movSpeed = movSpeed * -1;
     }
