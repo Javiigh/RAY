0e78dcfcacce4efc1f2fe95ac3ab8465651679d9
Javiigh <javiigh2001@gmail.com>
2022-02-16 08:55:49 +0000

    T CL 16/02

--- La aventura Ruby/Assets/Scripts/RubyController.cs
+++ La aventura Ruby/Assets/Scripts/RubyController.cs
@@ -16,9 +16,15 @@ public class RubyController : MonoBehaviour
 
     Rigidbody2D rigidbody2d;
 
+    Animator animator;
+    Vector2 lookDirection = new Vector2(1,0);
+
+    public GameObject projectilePrefab;
+
     void Start()
     {
         rigidbody2d = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
 
         currentHealth = maxHealth;
     }
@@ -28,9 +34,21 @@ public class RubyController : MonoBehaviour
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
 
+        Vector2 move = new Vector2(horizontal, vertical);
+
+        if (!Mathf.Approximately(move.x, 0.0f) || !Mathf.Approximately(move.y, 0.0f))
+        {
+            lookDirection.Set(move.x, move.y);
+            lookDirection.Normalize();
+        }
+
+        animator.SetFloat("Look X", lookDirection.x);
+        animator.SetFloat("Look Y", lookDirection.y);
+        animator.SetFloat("Speed", move.magnitude);
+
         Vector2 position = rigidbody2d.position;
-        position.x = position.x + speed * horizontal * Time.deltaTime;
-        position.y = position.y + speed * vertical * Time.deltaTime;
+
+        position = position + move * speed * Time.deltaTime;
 
         rigidbody2d.MovePosition(position);
 
@@ -51,11 +69,23 @@ public class RubyController : MonoBehaviour
 
             isInvincible = true;
             invincibleTimer = timeInvincible;
+
+            animator.SetTrigger("Hit");
         }
 
         currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
 
         Debug.Log(currentHealth + "/" + maxHealth);
+    }
+
+    void Launch()
+    {
+        GameObject projectileObject = Instantiate(projectilePrefab, rigidbody2d.position + Vector2.up * 0.5f, Quaternion.identity);
+
+        Projectile projectile = projectileObject.GetComponent<Projectile>();
+        projectile.Launch(lookDirection, 300);
+
+        animator.SetTrigger("Launch");
     }
 
 }
